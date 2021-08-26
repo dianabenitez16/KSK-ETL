dfa084a3a2c62b0e649fd5123b22fa0d150584a3
dianabenitez16 <dianamagali99@hotmail.com>
2021-08-26 14:18:53 -0400

    a woman solved this!

--- KSK-ETL/src/ksk/etl/KSKETL.java
+++ KSK-ETL/src/ksk/etl/KSKETL.java
@@ -16,7 +16,7 @@ public class KSKETL {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-          System.out.println("logic");
+          System.out.println("logica");
     }
     
 }
