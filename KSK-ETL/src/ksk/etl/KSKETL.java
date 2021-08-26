d5b20b214dd42e5d830965d57b5a5c087e3c2b4b
dianabenitez16 <dianamagali99@hotmail.com>
2021-08-26 13:44:03 -0400

    nuevo

--- KSK-ETL/src/ksk/etl/KSKETL.java
+++ KSK-ETL/src/ksk/etl/KSKETL.java
@@ -16,7 +16,7 @@ public class KSKETL {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-          System.out.println("ola");
+          System.out.println("logic");
     }
     
 }
